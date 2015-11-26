eff00e69707cd403012c11ad5532e802713d2add
TheStigsGitCousin <david.i.fagerlund@gmail.com>
2015-11-26 11:32:35 +0100

    Added password

--- src/homework3/OwnerImpl.java
+++ src/homework3/OwnerImpl.java
@@ -6,9 +6,17 @@
 package homework3;
 
 import bank.Account;
+import java.math.BigInteger;
 import views.ClientPanel;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 /**
  *
@@ -17,11 +25,54 @@ import java.rmi.server.UnicastRemoteObject;
 public class OwnerImpl extends UnicastRemoteObject implements Owner {
     
     private String name;
+    private String password;
     private Account account;
     
-    public OwnerImpl(String name, Account account) throws RemoteException{
+    public OwnerImpl(String name, String password, Account account) throws RemoteException, Exception{
         this.name=name;
+        try {
+            this.password=generateStorngPasswordHash(password);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(OwnerImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new Exception();
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(OwnerImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new Exception();
+        }
         this.account=account;
+    }
+    
+    private static String generateStorngPasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt().getBytes();
+         
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+     
+    private static String getSalt() throws NoSuchAlgorithmException
+    {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt.toString();
+    }
+     
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException
+    {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
+        }
     }
     
     @Override
@@ -45,6 +96,11 @@ public class OwnerImpl extends UnicastRemoteObject implements Owner {
     
     @Override
     public String getName() throws RemoteException {
+        return name;
+    }
+    
+    @Override
+    public String getPassword() throws RemoteException {
         return name;
     }
     
